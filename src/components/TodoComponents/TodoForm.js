56a04cfe80028907b85106e5a2c52fba3c28a9e5
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-24 18:00:00 -0400

    Add state and data to App, fix bug }

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Todo extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                <input></input>
+            </div>
+        )
+    }
+}
+
+export default TodoForm
